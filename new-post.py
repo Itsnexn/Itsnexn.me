283f5c1c90ebc844986b64479a5d790c7bed36ed
Itsnexn <Itsnexn@tuta.io>
2022-03-29 23:44:42 +0430

    Update

--- new-post.py
+++ new-post.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-import os, datetime
+from os import path, listdir, mkdir, system
+import datetime
 
 # Get script directory
-script_dir = os.path.dirname(os.path.realpath(__file__))
+script_dir = path.dirname(path.realpath(__file__))
+
 
 # ansi colors
 class bcolors:
@@ -12,26 +14,27 @@ class bcolors:
     BOLD = '\033[1m'
     RESET = '\033[0m'
 
-def main():
-    categories = []    
 
-    for i in os.listdir(script_dir + '/_contents'):
-        if os.path.isdir(script_dir + '/_contents/' + i):
+def main():
+    categories = []
+
+    for i in listdir(path.join(script_dir, "_contents")):
+        if path.isdir(path.join(script_dir, "_contents", i)):
             categories.append(i)
 
-    title = input(bcolors.BOLD + bcolors.ORANGE + 'Title: ' + bcolors.RESET)
-    author = input(bcolors.BOLD + bcolors.ORANGE + 'Author: ' + bcolors.RESET)
-    description = input(bcolors.BOLD + bcolors.ORANGE + 'Description: ' + bcolors.RESET)
+    title = input(f"{bcolors.BOLD}{bcolors.ORANGE}Title: {bcolors.RESET}")
+    author = input(f"{bcolors.BOLD}{bcolors.ORANGE}Author: {bcolors.RESET}")
+    description = input(f"{bcolors.BOLD}{bcolors.ORANGE}Description: {bcolors.RESET}")
     date = datetime.datetime.now().strftime("%s")
-    tags = input(bcolors.BOLD + bcolors.ORANGE + 'Tags [seperated by ","]: ' + bcolors.RESET)
-    tags = [ i for i in [i.strip() for i in tags.split(',')] if i]
+    tags = input(f"{bcolors.BOLD}{bcolors.ORANGE}Tags [separated with ',']: {bcolors.RESET}")
+    tags = [i for i in [i.strip() for i in tags.split(',')] if i]
 
     while True:
         for i, category in enumerate(categories, start=1):
             print(f"{i} - {bcolors.GREEN}{category}{bcolors.RESET}")
         print(f"{i+1} - {bcolors.ORANGE}New Category{bcolors.RESET}")
         try:
-            choice = input(bcolors.BOLD + bcolors.ORANGE + '>>> ' + bcolors.RESET)
+            choice = input(f"{bcolors.BOLD}{bcolors.ORANGE}>>> {bcolors.RESET}")
             if int(choice) <= len(categories):
                 category = categories[int(choice)-1]
                 break
@@ -42,9 +45,10 @@ def main():
                     continue
                 else:
                     try:
-                        os.mkdir(script_dir + '/_contents/' + category)
+                        mkdir(path.join(script_dir, '/_contents', category))
                     except:
-                        print(f"{bcolors.RED}Could not create category{bcolors.RESET}")
+                        print(
+                            f"{bcolors.RED}Could not create category{bcolors.RESET}")
                         exit(1)
                     print(f"{bcolors.GREEN}Category created{bcolors.RESET}")
                     break
@@ -57,8 +61,9 @@ def main():
             print(f"{bcolors.RED}Unexpected Error: {bcolors.RESET}\n{e}")
             exit(1)
     while True:
-        slug = input(bcolors.BOLD + bcolors.ORANGE + 'Slug [Spaces will be replaced with "-"]: ' + bcolors.RESET).replace(" ", "-")
-        if os.path.exists(f"{script_dir}/_contents/{category}/{slug}.md"):
+        slug = input(bcolors.BOLD + bcolors.ORANGE +
+                     'Slug [Spaces will be replaced with "-"]: ' + bcolors.RESET).replace(" ", "-")
+        if path.exists(f"{script_dir}/_contents/{category}/{slug}.md"):
             print(f"{bcolors.RED}File already exists{bcolors.RESET}")
             continue
         break
@@ -70,10 +75,13 @@ description: {description}
 date: {date}
 tags: {tags}
 ---
+
 """)
-    choice = input(f"{bcolors.GREEN}Open file in default $EDITOR? [y/n]: {bcolors.RESET}")
+    choice = input(
+        f"{bcolors.GREEN}Open file in default $EDITOR? [y/n]: {bcolors.RESET}")
     if choice.strip() == "y":
-        os.system(f"$EDITOR {script_dir}/_contents/{category}/{slug}.md")
+        system(f"$EDITOR {script_dir}/_contents/{category}/{slug}.md")
+
 
 if __name__ == '__main__':
     main()
